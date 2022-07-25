298410e8a68a350f97266601858dffc5bc82ac63
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2022-07-25 17:04:57 -0500

    Add files app api

---
+++ app/src/main/java/com/rucko/consumoapi/Comentario.kt
@@ -0,0 +1,12 @@
+package com.rucko.consumoapi
+import com.google.gson.annotations.SerializedName
+
+
+data class Comentario (
+    @SerializedName("postId") var postId:Int,
+    @SerializedName("id") var id:Int,
+    @SerializedName("name") var name:String,
+    @SerializedName("email") var email: String,
+    @SerializedName("body") var body: String
+    )
+
